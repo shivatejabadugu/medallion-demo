6eb6cd391eff7a561ccce7fbc1f805ef7c0e68e6
Shiva Badugu <shivatejabadugu@gmail.com>
2026-09-26 16:02:06 -0700

    add code to python file

--- pythonfile.py
+++ pythonfile.py
@@ -3,5 +3,12 @@ b = 2
 c = a+b
 print(c)
 
-Name = input("enter you name: ")
-age = int.input("enter your age: ")
+name = input("enter you name: ")
+age = int(input("enter your age: "))
+print(f"Hello, {name}")
+
+if age >= 18:
+    print(f"your age is {age}, welcome to the club")
+else:
+    print("your not allowed to the club")
+    print("Bye")
